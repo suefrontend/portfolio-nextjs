ae7c82c43aed2f36896f0bcf031c21ed2cbd56a4
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-26 21:33:11 -0700

    Apply font family

--- components/HighlightItem.jsx
+++ components/HighlightItem.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
 
-const HighlightItem = () => {
-  return <div>HighlightItem</div>;
+const HighlightItem = (props) => {
+  const { id, title, description, icon } = props;
+
+  return (
+    <div className="card text-center py-4 px-4">
+      <div>
+        <div>{icon}</div>
+        <h2 className="uppercase">{title}</h2>
+      </div>
+      <p>{description}</p>
+    </div>
+  );
 };
 
 export default HighlightItem;
