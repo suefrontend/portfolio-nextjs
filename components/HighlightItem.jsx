2d8e096323222d74fb973f0fd249685a64a0992a
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-26 22:36:42 -0700

    Set default text color

--- components/HighlightItem.jsx
+++ components/HighlightItem.jsx
@@ -1,17 +1,27 @@
 "use client";
+import { PiPencilLight } from "react-icons/pi";
+import { BiLogoReact } from "react-icons/bi";
+import { FiTool } from "react-icons/fi";
+import { PiWrenchLight } from "react-icons/pi";
 
-const HighlightItem = (props) => {
+function HighlightItem(props) {
   const { id, title, description, icon } = props;
 
+  const iconName = {
+    "ui-design": <PiPencilLight size={50} />,
+    "web-development": <BiLogoReact size={50} />,
+    "website-renewal": <PiWrenchLight size={50} />,
+  };
+
   return (
-    <div className="card text-center py-4 px-4">
-      <div>
-        <div>{icon}</div>
-        <h2 className="uppercase">{title}</h2>
+    <div className="card text-center p-4">
+      <div className="flex-column items-center justify-center py-10">
+        <div className="border-black border-2 rounded-full inline-block">{iconName[icon]}</div>
+        <h2 className="uppercase text-sm">{title}</h2>
       </div>
       <p>{description}</p>
     </div>
   );
-};
+}
 
 export default HighlightItem;
