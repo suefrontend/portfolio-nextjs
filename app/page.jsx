7e3bc67f44e428a613b34c1a2a07ad9a574e22ae
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-06-29 16:37:18 -0700

    Adjust main image size for different screen sizes

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect } from "react";
 import Image from "next/image";
 import Main from "@components/Main";
 import Highlight from "@components/Highlight";
@@ -5,16 +7,24 @@ import ProjectList from "@components/ProjectList";
 import About from "@components/About";
 import Contact from "@components/Contact";
 import Footer from "@components/Footer";
+import Nav from "@components/Nav";
 
 export default function Home() {
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      console.log("scrolled");
+    });
+  });
+
   return (
-    <div className="app">
+    <>
+      <Nav />
       <Main />
       <Highlight />
       <ProjectList />
       <About />
       <Contact />
       <Footer />
-    </div>
+    </>
   );
 }
--- components/Main.jsx
+++ components/Main.jsx
@@ -3,19 +3,15 @@ import Image from "next/image";
 
 function Main() {
   return (
-    <div className="lg:h-screen mb-20">
-      <Image
-        src="/images/spiral.jpg"
-        width={700}
-        height={700}
-        className="mx-auto mix-blend-darken"
-        alt="Spiral"
-      />
-      <div className="uppercase text-center">
-        <h1 className="text-3xl tracking-wider">Kayano Oyama</h1>
-        <span className="text-pink text-lg tracking-widest">
-          Software Developer
-        </span>
+    <div className="w-full h-auto border border-black max-lg:pt-28 lg:pt-60 lg:pb-16 max-lg:pb-12">
+      <div className="mx-auto relative max-md:h-60 max-lg:h-72 lg:h-96 border border-green-600">
+        <Image
+          src="/images/spiral-new.png"
+          // sizes="(max-width: 768px) 50vw"
+          fill
+          className="mx-auto mix-blend-darken border border-blue-600 object-contain"
+          alt="Spiral"
+        />
       </div>
     </div>
   );
